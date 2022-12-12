ffe88810dc0d4785e96123d5a99f70927def9ff9
MarcosChave <marcosvtc7@outlook.com>
2022-12-12 13:45:58 +0000

    Laout GE

---
+++ script.js
@@ -0,0 +1,10 @@
+let imagem = document.querySelector('.trocaImg')
+let text = document.querySelector('.h2')
+
+function trocaImagem(imgFundo) {
+  imagem.src = imgFundo
+}
+
+function trocaText(h2) {
+  text.innerText = h2
+}
